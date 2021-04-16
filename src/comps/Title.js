d4bedb86bcacd744b7c5e7ed06fdad577f46d19f
bharat2913 <bs987143@gmail.com>
2021-04-16 15:11:44 +0530

    docs: change name

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     return ( 
 
         <div className="title">
-            <h1> Firegram </h1>
+            <h1> Photogram </h1>
             {/* <h2>  Title </h2> */}
             <p>  Upload your photos here. </p>
         </div>
